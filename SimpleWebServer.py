72bb341a8c7b123d1e16b6c9d59591ab3ff3a814
ChopinNocture <chopin_7up@icloud.com>
2018-04-13 21:45:27 +0800

    change port

--- SimpleWebServer.py
+++ SimpleWebServer.py
@@ -1,6 +1,6 @@
 import socket
  
-HOST, PORT = '', 6444
+HOST, PORT = '', 8888
  
 listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -17,7 +17,7 @@ HTTP/1.1 200 OK
  
 Hello, World!
 """
-    client_connection.send(http_response.encode('utf-8'))   #sendall(http_response)
+    client_connection.send(http_response.encode())   #sendall(http_response)
     client_connection.close()
 
 print("end")
