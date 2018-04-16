40516ad53236ecdc4bc25567d2fffe3864fa5088
ChopinNocture <chopin_7up@icloud.com>
2018-04-17 02:09:23 +0800

    test over

--- SimpleWebServer.py
+++ SimpleWebServer.py
@@ -1,9 +1,9 @@
-import socket
- 
+import socket as skt
+
 HOST, PORT = '', 8888
- 
-listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+listen_socket = skt.socket(skt.AF_INET, skt.SOCK_STREAM)
+listen_socket.setsockopt(skt.SOL_SOCKET, skt.SO_REUSEADDR, 1)
 listen_socket.bind((HOST, PORT))
 listen_socket.listen(1)
 print('Serving HTTP on port %s ...' % PORT)
@@ -11,13 +11,13 @@ while True:
     client_connection, client_address = listen_socket.accept()
     request = client_connection.recv(1024)
     print(request)
- 
+
     http_response = """
 HTTP/1.1 200 OK
  
 Hello, World!
 """
-    client_connection.send(http_response.encode())   #sendall(http_response)
+    client_connection.send(http_response.encode())  # sendall(http_response)
     client_connection.close()
 
-print("end")
+# print("end")
