6ce0a5bb237f6533a788c562d2b11bc0ace3fb46
ChopinNocture <chopin_7up@icloud.com>
2018-04-23 21:51:02 +0800

    data base build

--- EduSite/TestApp/models.py
+++ EduSite/TestApp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+
+
 # Create your models here.
+
+class BlogArtis(models.Model):
+    title = models.CharField(max_length=300)
+    author = models.ForeignKey(User, related_name='blog_posts', on_delete=models.CASCADE,)
+    body = models.TextField()
+    publis = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        ordering = ("-publis",)
+
+    def __str__(self):
+        return self.title
