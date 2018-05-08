323f95c6a7f49f3cbd5b8ded07e7da915aa82952
ChopinNocture <chopin_7up@icloud.com>
2018-05-08 20:19:11 +0800

    Session Test

--- EduSite/TestApp/views.py
+++ EduSite/TestApp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def test_session(request):
+    sid = request.session['_auth_user_id']
+#    print(sid.len)
+    return HttpResponse("Testing Session: " +sid)
