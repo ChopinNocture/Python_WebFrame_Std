2cc11579fb89b8a815fd1069575902f5b4c04cf0
ChopinNocture <chopin_7up@icloud.com>
2018-05-18 21:25:20 +0800

    完成类型题目添加、修改、查看

--- EduSite/CourseFunApp/urls.py
+++ EduSite/CourseFunApp/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     path('questions/list/<slug:qtype>/', views.get_question_list),
     path('questions/editor/<slug:qtype>/', views.question_editor),
+    path('questions/editor/<slug:qtype>/<int:qid>/', views.question_editor),
 #    path('delete/<int:notice_id>/', views.delete_notice),
 #    path('get/', views.get_notices),
 ]
--- EduSite/CourseFunApp/views.py
+++ EduSite/CourseFunApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.forms import ModelForm
 
 # Create your views here.
 
@@ -21,17 +22,46 @@ Q_FORM_SUFFIX = 'Form'
 def get_question_list(request, qtype):
     #   package = __import__('\\.models')
     temp_class = getattr(questionModels, qtype + Q_TYPE_SUFFIX)
+    if not issubclass(temp_class, questionModels.Question):
+        return HttpResponse("Error type:" + qtype)
+
     questList = temp_class.objects.all()
     # return render(request=request, template_name="home.html" )
 
     return HttpResponse(questList)
 
 
-def question_editor(request, qtype):
+def question_editor(request, qtype, qid=-1):
     formClass = getattr(questionForms, qtype + Q_FORM_SUFFIX)
-    retForm = formClass()
 
-    # return HttpResponse(retForm)
-    return render(request=request, template_name="home.html", context={"form": retForm})
+    if not issubclass(formClass, ModelForm):
+        return HttpResponse("Error type:" + qtype)
+
+    requestData = getattr(request, request.method)
+
+    quest_in_DB = None
+    if qid is not -1:
+        temp_class = getattr(questionModels, qtype + Q_TYPE_SUFFIX)
+        if issubclass(temp_class, questionModels.Question):
+            quest_in_DB = temp_class.objects.get(id=qid)
+
+    if request.method == "POST":
+        newQuestForm = formClass(requestData, instance=quest_in_DB)
+
+        if newQuestForm.is_valid():
+            newQuestForm.save()
+#            quest_in_DB = newQuestForm.save(commit=False)
+#            formData = newQuestForm.cleaned_data
+#            for iter in newQuestForm.fields:
+#                print(iter)
+#                setattr(quest_in_DB, iter, formData[iter])
+#            quest_in_DB.save()
+            return HttpResponse("Succeed")
+        else:
+            return HttpResponse("False")
+
+    elif request.method == "GET":
+        retForm = formClass(instance=quest_in_DB)
+        return render(request=request, template_name="course/questionEditor.html", context={"form": retForm})
 
     # return HttpResponse(temp_class.get_url_name())
