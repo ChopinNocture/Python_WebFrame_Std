bedc22ae36c18550fa15b6f766951789b2a4c50b
ChopinNocture <chopin_7up@icloud.com>
2018-05-24 02:47:15 +0800

    嫦娥 field

--- EduSite/CourseFunApp/forms.py
+++ EduSite/CourseFunApp/forms.py
@@ -1,7 +1,6 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, HiddenInput
 
 from .models import *
-
 
 FIELD_LIST = ['description', 'sectionID', 'flag', 'star']
 
@@ -10,39 +9,54 @@ FIELD_LIST = ['description', 'sectionID', 'flag', 'star']
 class FillInBlankForm(ModelForm):
     class Meta:
         model = FillInBlankQuestion
-        fields = FIELD_LIST+['blankKeys']
+        fields = FIELD_LIST + ['blankKeys']
+        widgets = {
+            'blankKeys': HiddenInput()
+        }
 
 
 # 判断题
 class TrueOrFalseForm(ModelForm):
     class Meta:
         model = TrueOrFalseQuestion
-        fields = FIELD_LIST+['Key']
+        fields = FIELD_LIST + ['Key']
 
 
 # 单项选择题
 class ChoiceForm(ModelForm):
     class Meta:
         model = ChoiceQuestion
-        fields = FIELD_LIST+['options', 'key']
+        fields = FIELD_LIST + ['options', 'key']
+        widgets = {
+            'key': HiddenInput(attrs={'id': 'SubmitKey'})
+        }
+        labels = {
+            'key': 'SubmitKey'
+        }
+        help_texts = {
+            'key': '1'
+        }
 
 
 # 多项选择题
 class MultiChoiceForm(ModelForm):
     class Meta:
         model = MultiChoiceQuestion
-        fields = FIELD_LIST+['options', 'keys']
+        fields = FIELD_LIST + ['options', 'keys']
+        widgets = {
+            'keys': HiddenInput(attrs={'id': 'SubmitKey'})
+        }
 
 
 # 配对题
 class PairForm(ModelForm):
     class Meta:
         model = PairQuestion
-        fields = FIELD_LIST+['leftOptions', 'rightOptions', 'pairKeys']
+        fields = FIELD_LIST + ['leftOptions', 'rightOptions', 'pairKeys']
 
 
 # 排序题
 class SortForm(ModelForm):
     class Meta:
         model = SortQuestion
-        fields = FIELD_LIST+['options', 'keys']
+        fields = FIELD_LIST + ['options', 'keys']
