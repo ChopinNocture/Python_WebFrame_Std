279a7282743f083426a206b45824a1d2cb551272
ChopinNocture <chopin_7up@icloud.com>
2018-06-18 19:12:22 +0800

    student

--- EduSite/AccountApp/models.py
+++ EduSite/AccountApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class StudentProf(models.Model):
+    user = models.OneToOneField(User, unique=True)
+    gold = models.IntegerField()
+    progress
+    rank
