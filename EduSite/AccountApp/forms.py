01518c7283de5279b101c563c9bb4a212326be28
ChopinNocture <chopin_7up@icloud.com>
2018-07-19 04:42:44 +0800

    字体以及登录界面

--- EduSite/AccountApp/forms.py
+++ EduSite/AccountApp/forms.py
@@ -5,8 +5,9 @@ from .models import StudentProf
 # FIELD_LIST = ['description', 'sectionID', 'flag', 'star']
 
 class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(label='用户名', label_suffix=' ')
+    password = forms.CharField(widget=forms.PasswordInput, label='密码', label_suffix=' ')
+    
 
 class Student_Prof_Form(forms.ModelForm):
     class Meta:
