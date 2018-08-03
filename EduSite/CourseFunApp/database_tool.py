901f626da172110098d276cab11c6b5de9052757
ChopinNocture <chopin_7up@icloud.com>
2018-08-04 03:28:27 +0800

    切换 excel库

--- EduSite/CourseFunApp/database_tool.py
+++ EduSite/CourseFunApp/database_tool.py
@@ -1,31 +1,43 @@
+import openpyxl
 import CourseFunApp.models as questionModels
-import CourseFunApp.exam_system as exam_sys
-import xlwings
+
+# import CourseFunApp.exam_system as exam_sy
+
 
 def update_DB_from_excel(excel_url):
-    wb = xlwings.Book(excel_url)
+    lesson_list = questionModels.Lesson.objects.all()
 
-    config_sht = wb.sheets['config']
-    sht_name_list = config_sht.range('D2:D8').value
+    wb = openpyxl.load_workbook(filename=excel_url, read_only=True, data_only=True)
+    # sectionID
+    # description
+    config_sht = wb['config']
 
-    for sht_name in sht_name_list:
-        cur_sht = wb.sheets[sht_name]
+    for row in range(2, 8):
+        qTypeName = config_sht.cell(row=row, column=4).value
+
+        print("----" + str(qTypeName))
+        cur_sht = wb[qTypeName]
         if cur_sht is not None: 
             row_idx = 3
 
             while True:
                 idx_str = str(row_idx)
-                ques_desc = cur_sht.range('G' + idx_str).value
-                ques_type = cur_sht.range('F' + idx_str).value
+                ques_desc = cur_sht['G' + idx_str].value
+                ques_type = cur_sht['F' + idx_str].value
 
                 if ques_desc is not None and len(ques_desc)>0:
-                    print(ques_type)
+                    print(ques_desc)
                     try:
+                        print(ques_type)
                         temp_class = questionModels.get_qType_class(ques_type)
                     except (AttributeError) as e:
                         raise e
+
+                    quests = temp_class.objects.filter(sectionID=section_id).values('id', 'description', 'sectionID', 'flag')
                 else:
                     break
 
                 row_idx += 1
-        
+
+
+# update_DB_from_excel('D:/Temp/DB_1.xlsm')
