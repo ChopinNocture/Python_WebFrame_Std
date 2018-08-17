da79a551717f25105185ea52d0754f2139eb38c1
ChopinNocture <chopin_7up@icloud.com>
2018-08-18 00:19:48 +0800

    main style

--- EduSite/Statics/js/user/student_main.js
+++ EduSite/Statics/js/user/student_main.js
@@ -1,4 +1,3 @@
-
 $(document).ready(onInit);
 
 function onInit(event) {
@@ -9,7 +8,49 @@ function onInit(event) {
         success: onNoticesGet,
         error: failFunc
     });
+
+    $('#l_m').css('background-size', '100% 100%');
+    $('#move_prev').click(onPrev);
+    $('#move_next').click(onNext);
 }
+
+function movingAnim() {
+    $('#lesson_inner').addClass('anim-move');
+    $('#lesson_ctrl li').addClass('anim-move');
+    $('#lesson_ctrl span').addClass('anim-move');
+
+    $('#l_m').css('background-size', '80% 70%');
+
+    setTimeout(() => {
+        $('#lesson_inner').removeClass('anim-move');
+        $('#lesson_ctrl li').removeClass('anim-move').css('background-size', '80% 70%');
+        $('#lesson_ctrl span').removeClass('anim-move');
+
+        $('#lesson_inner').css('top', '-10rem');
+
+        $('#l_u span').css({'background-size':'80% 70%', 'opacity':'1'});
+        $('#l_b span').css({'background-size':'80% 70%', 'opacity':'1'});
+        $('#l_m').css('background-size', '100% 100%');
+        $('#l_m span').css({'background-size': '100% 100%','opacity':'0'});
+    }, 400);     
+}
+
+function onPrev(event) {      
+    movingAnim();
+    $('#l_m .lesson-book-u').css({'background-size': '80% 70%','opacity':'1'});
+    $('#l_b').css('background-size', '100% 100%');
+    $('#l_b span').css({'background-size':'100% 0%', 'opacity':'0'});
+    $('#lesson_inner').css('top', '-20rem');    
+}
+
+function onNext(event) {
+    movingAnim();
+    $('#l_m .lesson-book-b').css({'background-size': '80% 70%','opacity':'1'});
+    $('#l_u').css('background-size', '100% 100%');
+    $('#l_u span').css({'background-size':'100% 0%', 'opacity':'0'});
+    $('#lesson_inner').css('top', '0rem');
+}
+
 
 var notices_list = [];
 var cur_idx = 0;
