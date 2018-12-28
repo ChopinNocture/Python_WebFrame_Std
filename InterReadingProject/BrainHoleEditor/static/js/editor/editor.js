8482d4364329e3b0a194988ee65fa5cae3f06ffa
ChopinNocture <chopin_7up@icloud.com>
2018-12-29 00:11:52 +0800

    更新

---
+++ InterReadingProject/BrainHoleEditor/static/js/editor/editor.js
@@ -0,0 +1,21 @@
+Vue.component('ed-action', {
+    data: function () {
+        return 'action';
+    },
+    template:''
+})
+
+Vue.component('ed-sentence',{
+    data: function () {
+        return 'action';
+    }
+})
+
+Vue.component('ed-dialog',{})
+Vue.component('ed-decision',{})
+Vue.component('ed-game',{})
+
+
+Vue.component('ed-scene-show',{})
+Vue.component('ed-avatar-show',{})
+Vue.component('ed-buble-show',{})
