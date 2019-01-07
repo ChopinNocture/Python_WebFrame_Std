e6de18eb038c657da321237bdc77bdd17dcb311c
ChopinNocture <chopin_7up@icloud.com>
2019-01-07 22:34:42 +0800

    Update settings.py

--- EduSite/EduSite/settings.py
+++ EduSite/EduSite/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #'django_cas_ng.middleware.CASMiddleware',  # cas
+    'django_cas_ng.middleware.CASMiddleware',  # cas
 ]
 
 ROOT_URLCONF = 'EduSite.urls'
